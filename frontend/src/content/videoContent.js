c7763fd70b4c0da4f23f606eff0d3fa6e533752e
Lonath Gunawardena <lk.gcm29@gmail.com>
2025-01-10 18:36:18 +0530

    Update videoContent.js

--- frontend/src/content/videoContent.js
+++ frontend/src/content/videoContent.js
@@ -5,7 +5,7 @@ const videoContent = {
       description: "Awarded for the People's Pinnacle most outstanding sinhala & english medium teacher for the year 2024",
     },
     si: {
-      title: "ප්‍රශංසා",
+      title: "Achievements",
       subtitle: "අපගේ අභිමානවත් ජයග්‍රහණ",
       description: "People's Pinnacle 2024 වසරේ විශිෂ්ඨතම සිංහල හා ඉංග්‍රීසි මාධ්‍ය ගුරුවරයාට හිමි සම්මානයෙන් මෙසේ පිදුම් ලැබූ වගයි",
     },
